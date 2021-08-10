8e28fc92403725227664418e542366ca9c7ae564
nixon piñeres <nixonarmando8@gmail.com>
2021-08-10 15:28:05 -0500

    introduccion al manejo de rutas

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DestinoviajeComponent } from './destinoviaje/destinoviaje.component';
 import { ListadestinosComponent } from './listadestinos/listadestinos.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',component: ListadestinosComponent },
+  { path: 'destino',component:  DestinoDetalleComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoviajeComponent,
-    ListadestinosComponent
+    ListadestinosComponent,
+    DestinoDetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
